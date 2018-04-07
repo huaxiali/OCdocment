ec5780b402bf1f6868d925a37b9ffcd2d7d9f339
huaxiali <18236238639@163.com>
2018-04-07 10:21:58 +0800

    测试

---
+++ OCFirstGit/OCFirstGit/RootVC.swift
@@ -0,0 +1,49 @@
+//
+//  RootVC.swift
+//  OCFirstGit
+//
+//  Created by 一狂小生 on 2018/4/7.
+//  Copyright © 2018年 1226042260@qq.con. All rights reserved.
+//
+
+import UIKit
+
+typealias ClosureBlock = (String) -> Void
+
+class RootVC: UIViewController {
+
+    var block: ClosureBlock?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let disBtn = UIButton(frame: CGRect(x: 100, y: 100, width: 40, height: 40))
+        //(frame: CGRect(origin: CGPoint(x: ScreenWidth / 2, y: ScreenHeight / 2), size: CGSize(width: 40, height: 40)))
+        
+        disBtn.backgroundColor = UIColor.red
+        disBtn.addTarget(self, action: #selector(touchDone), for: .touchUpInside)
+        self.view.addSubview(disBtn)
+    }
+    
+    @objc func touchDone() {
+        self.navigationController?.popViewController(animated: true)
+        self.block!("oo")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- OCFirstGit/OCFirstGit/ViewController.swift
+++ OCFirstGit/OCFirstGit/ViewController.swift
@@ -8,15 +8,30 @@
 
 import UIKit
 
+let ScreenWidth = UIScreen.main.bounds.width
+let ScreenHeight = UIScreen.main.bounds.height
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.view.backgroundColor = UIColor.red
+        let disBtn = UIButton(frame: CGRect(origin: CGPoint(x: ScreenWidth / 2, y: ScreenHeight / 2), size: CGSize(width: 40, height: 40)))
+        disBtn.backgroundColor = UIColor.red
+        disBtn.addTarget(self, action: #selector(touchDone), for: .touchUpInside)
+        self.view.addSubview(disBtn)
+
     }
 
+    @objc func touchDone() {
+        let rootVC = RootVC()
+        rootVC.block = { (str) in
+            print("str:\(str)")
+        }
+        self.navigationController?.pushViewController(rootVC, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
